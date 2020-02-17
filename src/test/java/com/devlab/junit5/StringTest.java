edc7664dd908d0d5f56219e4fccbf1f02b80eb0b
Gabriel Valdes <gabrielandres.valdesduarte@maritz.com>
2020-02-17 12:15:46 -0800

    Added assertJ dependency

--- src/test/java/com/devlab/junit5/StringTest.java
+++ src/test/java/com/devlab/junit5/StringTest.java
@@ -2,12 +2,21 @@ package com.devlab.junit5;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class StringTest {
 
     @Test
-    void test(){
-        fail("Not implemented yet");
+    void test() {
+        int length = "ABCD".length();
+
+        assertThat(length).isEqualTo(4);
+
+        //Write test code
+        //Invoke method square(4)
+        //Checks in place -16
+
+
     }
 }
