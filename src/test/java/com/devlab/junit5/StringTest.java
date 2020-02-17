76c6f0eec92cbb85a337d1e4dd1800d288288b30
Gabriel Valdes <gabrielandres.valdesduarte@maritz.com>
2020-02-17 12:04:13 -0800

    Refactored packages

---
+++ src/test/java/com/devlab/junit5/StringTest.java
@@ -0,0 +1,13 @@
+package com.devlab.junit5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class StringTest {
+
+    @Test
+    void test(){
+        fail("Not implemented yet");
+    }
+}
